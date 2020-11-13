04b08c1ad62b41ac06fb5833924f03424a4ca074
diana2030 <diana.silina@inbox.lv>
2020-11-13 18:45:54 +0200

    Izdzēsts uzd1.py un testa faili parsaukti formata test_B.py

--- src/uzdA.py
+++ src/uzdA.py
@@ -1,14 +1,16 @@
-# ### A. Drošs dalījums
+"""   
+    A. Drošs dalījums
 
-#     Funkcija akceptē divus argumentus - skaiļus a un b,
-#     aprēķina to dalījumu un atgriež to. Ja skaiļus dalīt nedrīkst,
-#     atgriež 0.
+    Funkcija akceptē divus argumentus - skaiļus a un b,
+    aprēķina to dalījumu un atgriež to. Ja skaiļus dalīt nedrīkst,
+    atgriež 0.
 
-#     Argumenti:
-#         a {int vai float} -- pirmais skaitlis
-#         b {int vai float} -- otrais skaitlis
-#     Atgriež:
-#         int vai float -- rezultāts
+    Argumenti:
+        a {int vai float} -- pirmais skaitlis
+        b {int vai float} -- otrais skaitlis
+    Atgriež:
+        int vai float -- rezultāts
+"""
 
 def dross_dalijums(a, b):
     if b != 0:
